71c6bdec5de4ae481d7b20e42cd0b33c4a8ff5cd
zatzoid <zatzoid@gmail.com>
2024-10-21 09:34:35 +0300

    phone

--- src/js/utils/forms.js
+++ src/js/utils/forms.js
@@ -2,9 +2,9 @@ import $ from 'jquery'
 import Form from "./Form";
 import Inputmask from "inputmask";
 
-const ru = new Inputmask('+7 (999) 999-99-99',{placeholder: ''}),
-    br = new Inputmask('+375 (999) 999-99-99',{placeholder: ''}),
-    kz = new Inputmask('+7 (999) 999-99-99',{placeholder: ''}),
+const ru = new Inputmask('+7 (999) 999-99-99', { placeholder: '' }),
+    br = new Inputmask('+375 (999) 999-99-99', { placeholder: '' }),
+    kz = new Inputmask('+7 (999) 999-99-99', { placeholder: '' }),
     phoneMap = {
         ru, br, kz
     }
@@ -15,7 +15,7 @@ export default function initForms() {
         forms.forEach((e) => {
             new Form(e, formSubmit)
             ru.mask($(e).find('input[name="phone"]'));
-            changeInputMask()
+            changeInputMask(e)
         })
     }
 }
@@ -24,17 +24,17 @@ function formSubmit(inputData) {
     console.log(inputData);
 }
 function changeInputMask(form) {
-  
+
     $(form).find('.input-phone__select-list-btn').on('click', (e) => {
-   
+        e.preventDefault()
         const { mask } = e.target.dataset,
             parent = $(e.target.closest('.input-phone')),
             imageLink = e.target.children[0].getAttribute('src')
-     /**
-         * 1. менает изображение 
-         * 2. применяет маску из хэша
-         * 3. закрывается
-         */
+        /**
+            * 1. менает изображение 
+            * 2. применяет маску из хэша
+            * 3. закрывается
+            */
         $('.input-phone__select-current-flag').attr('src', imageLink)
         phoneMap[mask].mask(parent.find('input[name="phone"]'))
         parent.find('.drop-down-target').trigger('click')
